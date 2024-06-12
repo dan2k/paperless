76229e16b700ce17d26866e7c86c94794b95f319
Seangtong Huankhai <seangtong.h@cdg.co.th>
2024-06-12 10:59:41 +0700

    remove title on index.vue

--- src/views/report/index.vue
+++ src/views/report/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <h5 class="text-center">รายงานการบำรุงรักษาระบบคอมพิวเตอร์</h5>
+    <!-- <h5 class="text-center">รายงานการบำรุงรักษาระบบคอมพิวเตอร์</h5> -->
     <div class="container alert alert-info">
         <div class="mb-1">
             <label for="exampleInputEmail1" class="form-label m-0">โครงการ:</label>
